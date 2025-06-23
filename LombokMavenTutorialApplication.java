ad5ac804c43a55235e4c4b179bd0ac6028dcd1d2
Shorabhkumar Gupta <shorabhkumar.gupta@valtech.com>
2025-06-23 17:13:40 +0530

    Adding new file test

---
+++ LombokMavenTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.lombokmaventutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokMavenTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokMavenTutorialApplication.class, args);
+    }
+
+}
