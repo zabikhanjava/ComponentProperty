e9e38a3e3246a7501e020559da53e49452644b81
IsmailZabiullaKhan <ismail.zabiulla@valtech.com>
2025-08-05 12:54:54 +0530

    change indent

--- src/main/java/com/example/interview/Product.java
+++ src/main/java/com/example/interview/Product.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Product {
-  private int product_id;
+public class Product { private int product_id;
 
   private double price;
 
